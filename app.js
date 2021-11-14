503797236d8a067b0fbcfc99b718f047ce282db5
Your Name <fernandezgermannicolas@gmail.com>
2021-11-14 18:58:53 -0300

    pruebageroku

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3030;
-app.use(express.static("public"));
+process.env.PWD = process.cwd()
+
+app.use(express.static(process.env.PWD + '/public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
