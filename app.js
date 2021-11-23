37d499ab3b99b15f3b9974d4196de187872c6940
Your Name <fernandezgermannicolas@gmail.com>
2021-11-22 23:02:54 -0300

    pruebaheroku2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const express = require('express')
 const app = express();
 const path = require("path");
 const port = 4000;
+
 app.use(express.static("public"));
 process.env.PWD = process.cwd()
 
@@ -10,9 +12,7 @@ app.listen(process.env.PORT || 4000, ()=>{
 
 app.use(express.static(process.env.PWD + '/public'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))});
 app.get("/", (req, res) => {
