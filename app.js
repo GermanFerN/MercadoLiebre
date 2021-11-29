5dbe90a08606a8457e5b46a20656eac61faa8ea4
Your Name <fernandezgermannicolas@gmail.com>
2021-11-29 20:05:15 -0300

    mercadoliebrefinal

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-const express = require('express')
-const app = express();
+var express = require('express')
 const path = require("path");
-const port = 4000;
 
-app.use(express.static("public"));
-process.env.PWD = process.cwd()
 
-app.listen(process.env.PORT || 4000, ()=>{
-    console.log('Servidor funcionando');
+var app = express()
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen( 4000, ()=>{
+    console.log('http://localhost:4000');
 });
 
-app.use(express.static(process.env.PWD + '/public'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/views/home.html"))});
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"))});
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "/views/login.html"))});
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/ofertas.html"))});  
- app.get("/", (req, res) => {
-     res.sendFile(path.join(__dirname, "/views/tiendasoficiales.html"))});       
-app.get("/", (req, res) => {
-     res.sendFile(path.join(__dirname, "/views/vender.html"))});
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/ayuda.html"))});
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html"))});
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/views/login.html"))});
+
+app.get('/ayuda', function(req, res) {
+        res.sendFile(path.join(__dirname, "/public/views/ayuda.html"))});
+        
+app.get('/miscompras', function(req, res) {
+         res.sendFile(path.join(__dirname, "/public/views/miscompras.html"))});
+
+app.get('/ofertas', function(req, res) {
+          res.sendFile(path.join(__dirname, "/public/views/ofertas.html"))});
+                
+app.get('/register', function(req, res) {
+         res.sendFile(path.join(__dirname, "/public/views/register.html"))});
+
+app.get('/', function(req, res) {
+         res.sendFile(path.join(__dirname, "/public/views/tiendasoficiales.html"))});
+
+app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, "/public/views/vender.html"))});
+
+    
